8389799906dc79059abd79bdbd3988e8a6b419ae
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-11-25 11:23:10 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+      even_numbers = []
+      for num in num_list:
+           if num % 2 == 0:
+                even_numbers.append(num)
+      return even_numbers if even_numbers else []
 
 def make_exclamation(sentence_list):
-    pass
+     for i in range(len(sentence_list)):
+        if len(sentence_list) > 0:
+            sentence_list[i] += "!"
+     return sentence_list if sentence_list else []
